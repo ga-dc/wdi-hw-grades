06a1957665286587340f1843a1a26ee84dc786a5
Jesse Shawl <jesse@jshawl.com>
2015-01-12 10:28:01 -0500

    wip

---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'rest-client'
+require 'json'
+require './env' if File.exists?('env.rb')
+
+enable :sessions
+set :session_secret, ENV['GH_SESSION_SECRET']
+
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+URL = ENV['GH_URL']
+
+get '/' do
+  session['access_token'] ||= ''
+  if session['user_name']
+    @grade = '73%'
+  end
+  @client_id = CLIENT_ID
+  @access_token = session['access_token']
+  @url = URL
+  @user_name = session['user_name']
+  @avatar_url = session['avatar_url']
+  erb :index
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/')
+end
+
+get '/callback' do
+  session_code = request.env['rack.request.query_hash']['code']
+  result = RestClient.post('https://github.com/login/oauth/access_token', {
+      :client_id => CLIENT_ID,
+      :client_secret => CLIENT_SECRET,
+      :code => session_code
+  },  :accept => :json)
+  session['access_token'] = JSON.parse(result)['access_token']
+  user = JSON.parse(RestClient.get('https://api.github.com/user?access_token=' + session['access_token']))
+  session['user_name'] = user['login']
+  session['avatar_url'] = user['avatar_url']
+  redirect to('/');
+end
